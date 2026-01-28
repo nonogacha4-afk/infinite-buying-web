335773bc8fae29cbe392ff66831aa18dcd962e6b
nonogacha4-afk <nonogacha4@gmail.com>
2026-01-28 22:54:50 +0900

    Sync src/components/TradeLogManager.jsx

--- src/components/TradeLogManager.jsx
+++ src/components/TradeLogManager.jsx
@@ -17,7 +17,7 @@ const TradeLogManager = ({ logs, onAdd, onClear, t }) => {
                             <th>{t('logDate')}</th>
                             <th>{t('logPrice')}</th>
                             <th>{t('logQty')}</th>
-                            <th>{t('logAmount')} (₩)</th>
+                            <th>{t('logAmount')} (??</th>
                             <th style={{ textAlign: 'right' }}>{t('logFx')}</th>
                         </tr>
                     </thead>
